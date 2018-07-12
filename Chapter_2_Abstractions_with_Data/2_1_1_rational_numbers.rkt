15022a42902bd2bdf7788eec3c29f7f45e5cb7c7
theJollySin <thejollysin@gmail.com>
2018-07-11 21:09:03 -0400

    rational numbers

--- Chapter_2_Abstractions_with_Data/2_1_1_rational_numbers.rkt
+++ Chapter_2_Abstractions_with_Data/2_1_1_rational_numbers.rkt
@@ -1,2 +1,24 @@
 #lang racket
 
+
+; builds a rational fraction from two integers
+; TODO: reduce this ratio using GCDs
+(define (make-rat n d) (cons n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))
+  )
+)
+
