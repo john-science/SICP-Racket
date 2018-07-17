0ecec6a5b659a9a3681a4a36c32cfce2f4c54f1a
theJollySin <thejollysin@gmail.com>
2018-07-16 20:30:05 -0400

    flatten tree into list

--- Chapter_2_Abstractions_with_Data/2_2_2_lists_as_trees.rkt
+++ Chapter_2_Abstractions_with_Data/2_2_2_lists_as_trees.rkt
@@ -2,28 +2,51 @@
 
 ; count the number of leaves in the tree/list
 ; original, tail-recursive version from the book
-(define (count-leaves0 x)
+(define (count-leaves x)
   (cond
     ((null? x) 0)
-    ((not (pair x)) 1)
-    (else (+ (count-leaves0 (car x))
-             (count-leaves0 (cdr x))
+    ((not (pair? x)) 1)
+    (else (+ (count-leaves (car x))
+             (count-leaves (cdr x))
           )
     )
   )
 )
 
-; count the number of leaves in the tree/list
-; modified from the book to NOT be tail-recursive
-(define (count-leaves tree)
-  (define (count_leaves-iter x n)
-    (cond
-      ((null? x) 0)
-      ((not (pair x)) 1)
-       (else (count-leaves-iter (car x) n)
-             (count-leaves-iter (cdr x) n)
-      )
+
+(display "\n(count-leaves (list 1 2 3 4)):  ")
+(display (count-leaves (list 1 2 3 4)))
+
+(display "\n(count-leaves (list (list 1 2) 3 4)):  ")
+(display (count-leaves (list (list 1 2) 3 4)))
+
+(display "\n(count-leaves (list (list 1 2) (list 3 4))):  ")
+(display (count-leaves (list (list 1 2) (list 3 4))))
+(newline)
+
+
+
+; flatten a tree into an ordered list of the leaves (left-to-right)
+(define (fringe x)
+  (cond
+    ((null? x) '())
+    ((not (pair? x)) (list x))
+    (else (append (fringe (car x))
+                  (fringe (cdr x))
+          )
     )
   )
-  (count-leaves tree 0)
 )
+
+
+
+(display "\n(fringe (list (list 1 2) 3 4)):  ")
+(display (fringe (list (list 1 2) 3 4)))
+
+(display "\n(fringe (list (list 1 2) (list (list 3 5 6 7)) 8)):  ")
+(display (fringe (list (list 1 2) (list (list 3 5 6 7)) 8)))
+
+(display "\n(fringe (list (list (list 1 2) (list (list 3 5 6 7))))):  ")
+(display (fringe (list (list (list 1 2) (list (list 3 5 6 7))))))
+(newline)
+(newline)
