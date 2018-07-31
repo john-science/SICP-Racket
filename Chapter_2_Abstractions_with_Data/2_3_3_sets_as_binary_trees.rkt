916c92d5b13f9f063d4f3e92877c8c25b44f3d70
theJollySin <thejollysin@gmail.com>
2018-07-30 20:09:02 -0400

    sets as trees

--- Chapter_2_Abstractions_with_Data/2_3_3_sets_as_binary_trees.rkt
+++ Chapter_2_Abstractions_with_Data/2_3_3_sets_as_binary_trees.rkt
@@ -28,3 +28,54 @@
 )
 
 
+;; add an element to a set
+(define (adjoin-set x set)
+  (cond
+    ((null? set) (make-tree x '() '()))
+    ((= x (entry set)) set)
+    ((< x (entry set))
+      (make-tree (entry set)
+                 (adjoin-set x (left-branch set))
+                 (right-branch set)
+      )
+    )
+    ((> x (entry set))
+      (make-tree (entry set)
+                 (left-branch set)
+                 (adjoin-set x (right-branch set))
+      )
+    )
+  )
+)
+
+
+
+;; Okay, well, let's play with our new tools some
+(define (a) (make-tree 7 '(4) '(13)))
+(define (b) (make-tree 70 '(54) '(313)))
+(define (c) (make-tree 23 (a) (b)))
+
+(display "a: ")
+(display (a))
+(newline)
+(display "b: ")
+(display (b))
+(newline)
+(display "c: ")
+(display (c))
+(newline)
+(newline)
+
+(define x (adjoin-set 10 '()))
+(define y (adjoin-set 7 x))
+(define z (adjoin-set 17 y))
+
+(display "x: ")
+(display x)
+(newline)
+(display "y: ")
+(display y)
+(newline)
+(display "z: ")
+(display z)
+(newline)
